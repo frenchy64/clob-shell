3904c8aa3f117d5c450639b9aa2b7b1ca6213d12
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-12-14 03:17:43 -0600

    wip

--- readline/prep.clj
+++ readline/prep.clj
@@ -2,10 +2,11 @@
   (:refer-clojure :exclude [compile])
   (:require [clojure.tools.build.api :as b]))
 
-(def class-dir "target/classes")
-(def basis (b/create-basis {:project "deps.edn"}))
+(def class-dir "classes")
+(def basis (delay (b/create-basis {:project "deps.edn"})))
 
 (defn compile [_]
   (b/compile-clj {:src-dirs ["src"]
+                  :ns-compile '[clob.readline.line-reader-class]
                   :class-dir class-dir
-                  :basis basis}))
+                  :basis @basis}))
